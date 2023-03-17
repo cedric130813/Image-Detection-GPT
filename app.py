bd8f3ff6e95b1fa4c62b174fb150e4ed3dbb3901
Cedric <82087099+cedric130813@users.noreply.github.com>
2023-03-17 18:58:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,8 @@ def index():
             openai.api_key = ("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")
             response = openai.Completion.create(
                 model="text-curie-001", #text-curie-001,text-davinci-003 https://beta.openai.com/docs/models/gpt-3
-                prompt=f"I want you to act as an retail recommendation system. I will provide you with a list of recommended items, and you will suggest 5 similar items including its brand that a customer could buy in Singapore based on those items, of different brands and bracket with its retail price in Singapore dollars. My suggestion request is {im_class}",
-                temperature=0.5,
+                prompt=f"I want you to act as an retail recommendation system. You will suggest 5 similar items including its brand that a customer could buy in Singapore based on the item given, of different brands and bracket with its retail price in Singapore dollars. My suggestion request is {im_class}",
+                temperature=0.7,
                 max_tokens=400)
             text = response['choices'][0]['text']
             return(render_template("index.html",result=text, img_url=url))
