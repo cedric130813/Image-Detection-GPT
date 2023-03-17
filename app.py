4fc3f709b1fc996c6f68726c3800ad76c80f9ea5
Cedric <82087099+cedric130813@users.noreply.github.com>
2023-03-17 11:10:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,5 +39,4 @@ def index():
         return(render_template("index.html",result="waiting"))
 
 if __name__=="__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(port=port)
+    app.run(port=8000)
