6e12a0be4cf09ab694ab97e80f5e8b1e756c0423
Cedric <82087099+cedric130813@users.noreply.github.com>
2023-03-17 17:52:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask,render_template,request
 import numpy as np
 import keras.utils as image
-from keras.applications import xception
+from keras.models import load_model
+from keras.applications.xception import Xception, preprocess_input,decode_predictions
+import matplotlib.pyplot as plt
+from pathlib import Path
 import openai
+import os
 import urllib.request
 
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
+    result = None #added
     img_path = None
     if request.method == "POST":
         url = request.form.get("urlpath")
@@ -16,19 +21,19 @@ def index():
             try:
                 img_path, headers = urllib.request.urlretrieve(str(url))
             except:
-                pass
+                result = None
         if img_path:
+            model = load_model('xception_model.h5')
             img = image.load_img(img_path, target_size=(299, 299))
             image_array = image.img_to_array(img)
             x_train = np.expand_dims(image_array, axis=0)
-            x_train = xception.preprocess_input(x_train)
-            model = xception.Xception(weights="imagenet")
+            x_train = preprocess_input(x_train)
             prediction = model.predict(x_train)
-            pred = xception.decode_predictions(prediction)[0][0]
+            im_class = np.argmax(prediction[0], axis=-1)
             openai.api_key = ("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")
             response = openai.Completion.create(
                 model="text-curie-001", #text-curie-001,text-davinci-003 https://beta.openai.com/docs/models/gpt-3
-                prompt=f"I want you to act as an retail recommendation system. I will provide you with a list of recommended items, and you will suggest 5 similar items including its brand that a customer could buy in Singapore based on those items, of different brands and bracket with its retail price in Singapore dollars. My suggestion request is {pred[1]}",
+                prompt=f"I want you to act as an retail recommendation system. I will provide you with a list of recommended items, and you will suggest 5 similar items including its brand that a customer could buy in Singapore based on those items, of different brands and bracket with its retail price in Singapore dollars. My suggestion request is {im_class}",
                 temperature=0.5,
                 max_tokens=400)
             text = response['choices'][0]['text']
